c40a6555f10ff78cad17c99250030033d5c5f2e9
Manuel Ortiz <manuel.ortiz@fluxit.com.ar>
2015-12-15 14:13:28 -0300

    Commit inicial del ejemplo completo con jhipster

---
+++ src/main/java/org/prueba/config/liquibase/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Liquibase specific code.
+ */
+package org.prueba.config.liquibase;
---
+++ src/main/java/org/prueba/web/websocket/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * WebSocket services, using Spring Websocket.
+ */
+package org.prueba.web.websocket;
